31e735bed4f6d1d386733dc674d0f00da0747f8c
Miroszlav <miroszlav.popovics@gmail.com>
2025-03-18 16:06:27 +0200

    faq-fixed

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,25 @@
 export function faq() {
-  document.querySelectorAll('.faq-item').forEach(item => {
-    item.addEventListener('click', () => {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    item.addEventListener('click', event => {
+      event.stopPropagation();
       item.classList.toggle('open');
       let text = item.querySelector('.faq-text');
-      if (text.style.display === 'block') {
-        text.style.display = 'none';
+      if (item.classList.contains('open')) {
+        text.style.maxHeight = text.scrollHeight + 'px';
       } else {
-        text.style.display = 'block';
+        text.style.maxHeight = '0';
+      }
+    });
+  });
+
+  document.addEventListener('click', event => {
+    faqItems.forEach(item => {
+      if (item.classList.contains('open')) {
+        item.classList.remove('open');
+        let text = item.querySelector('.faq-text');
+        text.style.maxHeight = '0';
       }
     });
   });
