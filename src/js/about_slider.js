67be81f6b6bd05991508d2b4d589fc6119ca6d66
Nikitin <nikitinigor0907@gmail.com>
2025-03-20 03:06:27 +0100

    fix:scrolling

--- src/js/about_slider.js
+++ src/js/about_slider.js
@@ -18,29 +18,46 @@ export function about_slider() {
     });
   });
 
-  let touchStartX, touchEndX;
+  let touchStartX, touchEndX, touchStartY, touchEndY;
+  let isSwipingHorizontal = false;
+  const swipeThreshold = 30;
 
   list.addEventListener('touchstart', e => {
     touchStartX = e.touches[0].clientX;
+    touchStartY = e.touches[0].clientY;
+    isSwipingHorizontal = false;
+  });
 
-    if (e.target.closest('.about-list')) {
+  list.addEventListener('touchmove', e => {
+    touchEndX = e.touches[0].clientX;
+    touchEndY = e.touches[0].clientY;
+
+    const horizontalDistance = Math.abs(touchEndX - touchStartX);
+    const verticalDistance = Math.abs(touchEndY - touchStartY);
+
+    if (
+      horizontalDistance > verticalDistance &&
+      horizontalDistance > swipeThreshold
+    ) {
+      isSwipingHorizontal = true;
       e.preventDefault();
+    } else {
+      isSwipingHorizontal = false;
     }
   });
 
   list.addEventListener('touchend', e => {
-    touchEndX = e.changedTouches[0].clientX;
-    handleSwipe();
-  });
-
-  function handleSwipe() {
-    if (touchEndX < touchStartX) {
-      index = (index + 1) % totalSlides;
-    } else if (touchEndX > touchStartX) {
-      index = (index - 1 + totalSlides) % totalSlides;
+    if (isSwipingHorizontal) {
+      if (touchEndX < touchStartX) {
+        // Swipe left
+        index = (index + 1) % totalSlides;
+      } else if (touchEndX > touchStartX) {
+        // Swipe right
+        index = (index - 1 + totalSlides) % totalSlides;
+      }
+      updateSlider();
     }
-    updateSlider();
-  }
+  });
 
   updateSlider();
 }
